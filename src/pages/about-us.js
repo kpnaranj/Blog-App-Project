3064459394942a6a97015f2dcc4104208ed13ab2
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-16 17:40:13 -0500

    Correction of commits and development of about-us and index

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -1,17 +1,15 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function About() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Home Page</title>
+        <title>About Us - Page</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>main</main>
-
-      <footer className={styles.footer}>footer</footer>
+      <p>This is the About us Page</p>
     </div>
   );
 }
