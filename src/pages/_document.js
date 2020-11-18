8e78d3d01ef498fab89af63d0e65cdb8b235c13f
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-18 14:55:52 -0500

    Improving theme

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
-import theme from "../styles/global-theme/theme";
+import theme from "../styles/global/theme";
 
 export default class MyDocument extends Document {
   render() {
