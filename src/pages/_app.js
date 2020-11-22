332f955a1dc92d14f723ce894a65dfe1b0402187
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-22 00:49:32 -0500

    Setting frontend from 0 to start project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,5 @@
-import { useEffect, Fragment } from "react";
-import PropTypes from "prop-types";
-import Head from "next/head";
-import { ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import theme from "../styles/global/theme";
-
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
-  useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector("#jss-server-side");
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-
-  return (
-    <Fragment>
-      <Head>
-        <title>Blog App pages</title>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width"
-        />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Fragment>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
 
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
+export default MyApp;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,27 +1,7 @@
-import { useStyles } from "../styles/home/Home";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
+import React from "react";
 
-export default function Home() {
-  const classes = useStyles();
-
-  return (
-    <Card>
-      <Typography variant="h6" className={classes.title}>
-        Home Page
-      </Typography>
-      <CardMedia
-        className={classes.media}
-        image="/img/unicorn-bike.jpg"
-        title="Unicorn Bicycle"
-      />
-      <CardContent>
-        <Typography variant="body2" component="p">
-          Welcome to the Next JS home page, this is awesome
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+function Home() {
+  return <div>This is the Home Page</div>;
 }
+
+export default Home;
