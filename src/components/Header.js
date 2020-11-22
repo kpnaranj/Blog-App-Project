0ac1b150123d2fe8ae312c6a23a45f261aa03efd
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-22 13:18:13 -0500

    Adding env variables to next js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { APP_NAME } from "../../config";
 import {
   Collapse,
   Navbar,
@@ -21,7 +22,7 @@ function Header() {
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand href="/">{APP_NAME}</NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
