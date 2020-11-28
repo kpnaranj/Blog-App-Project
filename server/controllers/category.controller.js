2e42ac321af10c4d95572dde3c492b51978093e6
Kevin Narsh <kpnaranj@uwaterloo.ca>
2020-11-28 04:08:01 -0500

    Succesfully adding routes create, list, read and remove for categories

--- server/controllers/category.controller.js
+++ server/controllers/category.controller.js
@@ -17,3 +17,42 @@ export const create = (req, res) => {
     res.json(data);
   });
 };
+
+export const list = (req, res) => {
+  Category.find({}).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(data);
+  });
+};
+
+export const read = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+
+  Category.findOne({ slug }).exec((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json(category);
+  });
+};
+
+export const remove = (req, res) => {
+  const slug = req.params.slug.toLowerCase();
+
+  Category.findOneAndRemove({ slug }).exec((err, data) => {
+    if (err) {
+      return res.status(400).json({
+        error: errorHandler(err),
+      });
+    }
+    res.json({
+      message: "Category deleted successfully",
+    });
+  });
+};
--- server/routes/category.routes.js
+++ server/routes/category.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 const router = Router();
-import { create} from "../controllers/category.controller";
+import { create, list, read , remove} from "../controllers/category.controller";
 
 // validators
 import { runValidation } from "../validators";
 import { categoryCreateValidator } from "../validators/category";
 import { requireSignin, adminMiddleware } from "../controllers/auth.controller";
-
+//Create a category
 router.post(
   "/category",
   categoryCreateValidator,
@@ -15,6 +15,11 @@ router.post(
   adminMiddleware,
   create
 );
-
+//Get a list of categories
+router.get("/categories", list);
+//Get single category
+router.get("/category/:slug", read);
+//Delete category
+router.delete("/category/:slug", remove);
 
 export default router;
